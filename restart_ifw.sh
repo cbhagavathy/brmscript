d077fcdbdbc82cf33e33df86429e19836b88c8d4
cbhagavathy <85443214+cbhagavathy@users.noreply.github.com>
2021-07-15 07:22:19 +0530

    Update restart_ifw.sh

--- restart_ifw.sh
+++ restart_ifw.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source ~/.bash_profile
+
 echo -n "Getting RT pipeline process id : "
 pid=`ps -eaf | grep reg | grep ifw | grep -v grep |awk ' { print $2 }'`
 
@@ -9,9 +11,10 @@ then
         kill -9 $pid
 
         echo "Restarting RT pipeline : "
-        cd $IFW_HOME
-        rm -rf conf/wirelessRealtime.reg.lock
-        bin/ifw -r  conf/wirelessRealtime.reg &
+        cd /opt/ifw
+        rm -rf /opt/ifw/conf/wirelessRealtime.reg.lock
+        nohup ./bin/ifw -r /opt/ifw/conf/wirelessRealtime.reg > restart_ifw.out 2> restart_ifw.err < /dev/null &
+
 else
         echo " No process running "
 fi
